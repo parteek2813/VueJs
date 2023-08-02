965261f1254bd2c93994dad311b98a5ec9625491
parteek kumar <digitalparteek@gmail.com>
2023-08-02 19:44:47 +0530

    Methods in params

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,6 +1,11 @@
 <template>
   <div class="hello">
     <h1>{{ msg }}</h1>
+    <h2>Email: {{ email }}</h2>
+    <h3>Name {{ getName() }}</h3>
+    <button @click="getEducation">Click me to get edu</button>
+    <h3>Sum of two numbers: {{ calculateSum(1, 5) }}</h3>
+    <h1>All data: {{ getData().email }}</h1>
   </div>
 </template>
 
@@ -9,6 +14,30 @@ export default {
   name: "HelloWorld",
   props: {
     msg: String,
+  },
+  data() {
+    return {
+      email: "Parteek1@gmail.com",
+      getName: function () {
+        return "Parteek kumar";
+      },
+    };
+  },
+
+  methods: {
+    getEducation() {
+      console.log("From education");
+      return "Btech";
+    },
+    calculateSum(num1, num2) {
+      return num1 + num2;
+    },
+    getData() {
+      return {
+        name: "Parteek",
+        email: this.email,
+      };
+    },
   },
 };
 </script>
