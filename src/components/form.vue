535cbb925dff3aa4c5b8772ea11c32a548cad30a
parteek kumar <digitalparteek@gmail.com>
2023-08-12 13:47:44 +0530

    complete form

--- src/components/form.vue
+++ src/components/form.vue
@@ -3,6 +3,7 @@
     <h1>Simple Form</h1>
     <form>
       <div>
+        <p>{{ form }}</p>
         <label for="email">Email</label>
         <!-- There are two methods of defining and getting the value from 
         the input box.... one way is to define [v-model=data.state or form.email] is this case
@@ -15,8 +16,7 @@
           type="text"
           id="email"
           placeholder="Enter email"
-          :value="form.email"
-          @input="form.email = $event.target.value"
+          v-model="form.email"
         />
       </div>
       <div>
@@ -25,10 +25,35 @@
           type="text"
           id="password"
           placeholder="Enter password"
-          :value="form.password"
-          @input="form.password = $event.target.value"
+          v-model="form.password"
         />
       </div>
+
+      <br />
+
+      <select v-model="form.country">
+        <option>India</option>
+        <option>China</option>
+        <option>Japan</option>
+      </select>
+
+      <br />
+
+      <h3>technology</h3>
+      <label>Java</label>
+      <input type="checkbox" value="Java" v-model="form.technology" />
+      <br />
+
+      <label>Kotlin</label>
+      <input type="checkbox" value="Kotlin" v-model="form.technology" />
+
+      <h3>Gender</h3>
+      <label>Male</label>
+      <input type="checkbox" value="Male" v-model="form.gender" />
+      <br />
+
+      <label>Female</label>
+      <input type="checkbox" value="Female" v-model="form.gender" />
 
       <button v-on:click="login" type="button">Login</button>
     </form>
@@ -43,6 +68,9 @@ export default {
       form: {
         email: "",
         password: "",
+        country: "",
+        technology: [],
+        gender: [],
       },
     };
   },
