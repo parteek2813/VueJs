507e5c5059591eba5ebb7a0dc2ce871cabc40738
parteek kumar <digitalparteek@gmail.com>
2023-08-13 15:40:24 +0530

    slots in vue

---
+++ src/components/Child1.vue
@@ -0,0 +1,14 @@
+<template lang="">
+   <h1>{{data}}</h1>
+   <slot></slot>
+</template>
+<script>
+export default {
+    name: "Child1",
+    
+
+}
+</script>
+<style scoped>
+    
+</style>
---
+++ src/components/Slots.vue
@@ -0,0 +1,30 @@
+<template lang="">
+<h1>Slots in Vue</h1>
+<Child1>
+    <h1>Code step by step</h1>
+</Child1>
+<Child1><a href="#">Code step by step</a></Child1>
+<Child1><img src="https://live.staticflickr.com/4561/38054606355_26429c884f_b.jpg" alt="Not rendered" /></Child1>
+</template>
+
+<script>
+import Child1 from "./Child1.vue"
+export default {
+    name: "Slots",
+    components: {
+        Child1
+    }
+
+}
+</script>
+
+<style lang="">
+
+</style>
+
+<!-- INterview question -->
+
+<!-- To set the default value of the slots we can have the default
+value in the slots tag in the child component. Due to this, if no value is passed
+from the parent component then a default value will be acquired from the child component 
+and then it will be used there in place of the slots  -->
