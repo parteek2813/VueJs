e23e8753dcc30005760f3f7a6e00d33cdb0c474d
parteek kumar <digitalparteek@gmail.com>
2023-08-14 15:57:13 +0530

    before update and update

--- src/components/User.vue
+++ src/components/User.vue
@@ -1,4 +1,4 @@
-<template>
+<!-- <template>
   <div>
     <h2>User component</h2>
     <button v-on:click="sendUserName">Send User Name</button>
@@ -25,4 +25,38 @@ export default {
 };
 </script>
 
-<style scoped></style>
+<style scoped></style> -->
+
+<template lang="">
+  <h1 ref="count">{{counter}}</h1>
+  <button v-on:click="counter=counter+1">Update Count</button>
+</template>
+<script>
+export default {
+  name: "User",
+  data(){
+    return {
+      counter: 0
+    }
+  },
+  beforeUpdate(){
+    console.log("BeforeUpdate", this.$refs["count"].textContent);
+  },
+  updated(){
+    console.log("updated", this.$refs["count"].textContent);
+  }
+}
+</script>
+<style lang="">
+</style>
+
+<!-- Interview question -->
+
+<!-- 
+  Does beforeUpdate and update calls on property value change or 
+  the UI value changes?
+
+  They call on the UI value changes and the difference betweent the 
+  two can be understood by the UI changes only.
+ -->
+
